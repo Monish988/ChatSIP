61b2f1192e9f18db4417ce3df8ec5ad4d35690ea
Monish Khan <monishkhan47786@gmail.com>
2026-02-02 19:11:35 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+import authRouter from "./routes/auth.route.js";
+import messagesRouter from "./routes/messages.route.js";
+import path from "path";
+
+const __dirname = path.resolve();
+const app = express();
+
+
+
+app.use('/api/auth',authRouter);
+app.use('/api/messages',messagesRouter);
+
+
+
+
+if(ENV.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+    })
+}
+
+async function startServer() {
+  app.listen(ENV.PORT, () => {
+    console.log("Server Started Successfully");
+  });
+}
+startServer();
